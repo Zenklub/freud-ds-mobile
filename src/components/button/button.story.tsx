7a0348ca5e09a05b55bf0553b2a5ff649b208ca4
erick <ericknmp@gmail.com>
2023-03-23 10:35:08 -0300

    feat(button): created button component

---
+++ src/components/button/button.story.tsx
@@ -0,0 +1,101 @@
+import { StoryWrapper } from '../../storybook/story-wrapper';
+import { Button } from './button';
+import { storiesOf } from '@storybook/react-native';
+import React from 'react';
+import { HStack } from 'native-base';
+import { Text } from '@components/typography/text';
+import { ButtonProps } from '@components/button/types';
+
+const sizes = ['lg', 'md', 'sm'] as const;
+const variants = ['solid', 'outline', 'ghost'] as const;
+
+const renderButtonSizes = (
+	size: 'lg' | 'md' | 'sm',
+	inverted: boolean,
+	extraProps: Partial<ButtonProps> = {},
+	key = 'default'
+) => {
+	const renderButton = (variant: 'solid' | 'outline' | 'ghost') => {
+		return (
+			<Button
+				size={size}
+				variant={variant}
+				inverted={inverted}
+				key={`${key}-${variant}`}
+				{...extraProps}
+			>
+				Button
+			</Button>
+		);
+	};
+
+	return (
+		<HStack
+			width="100%"
+			justifyContent="space-around"
+			key={`${key}-${size}-${inverted}`}
+		>
+			{variants.map(renderButton)}
+		</HStack>
+	);
+};
+
+const ButtonStory = () => {
+	const renderSession = (inverted: boolean) => {
+		return (
+			<StoryWrapper.Session
+				alignItems={undefined}
+				paddingX={3}
+				inverted={inverted}
+				key={`${inverted}`}
+			>
+				<HStack width="100%" justifyContent="space-around">
+					<Text inverted={inverted}>Solid</Text>
+					<Text inverted={inverted}>Outline</Text>
+					<Text inverted={inverted}>Ghost</Text>
+				</HStack>
+				{sizes.map((size) => renderButtonSizes(size, inverted))}
+
+				<Text marginTop={5} inverted={inverted}>
+					Width Icon
+				</Text>
+				{sizes.map((size) =>
+					renderButtonSizes(size, inverted, { icon: 'plus' }, 'icon')
+				)}
+				<Text marginTop={5} inverted={inverted}>
+					Disabled
+				</Text>
+				{sizes.map((size) =>
+					renderButtonSizes(size, inverted, { disabled: true }, 'disabled')
+				)}
+
+				<Text marginTop={5} inverted={inverted}>
+					Disabled Icon
+				</Text>
+				{sizes.map((size) =>
+					renderButtonSizes(
+						size,
+						inverted,
+						{ disabled: true, icon: 'plus' },
+						'disabled-icon'
+					)
+				)}
+				<Text marginTop={5} inverted={inverted}>
+					Loading
+				</Text>
+				{sizes.map((size) =>
+					renderButtonSizes(size, inverted, { isLoading: true }, 'loading')
+				)}
+			</StoryWrapper.Session>
+		);
+	};
+
+	return (
+		<StoryWrapper title="Forms | Button">
+			{renderSession(false)}
+			{renderSession(true)}
+		</StoryWrapper>
+	);
+};
+
+storiesOf('Forms', module).add('Button', () => <ButtonStory />);
