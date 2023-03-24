db003438d510bfe058f3a111feb0f6464da78158
erick <ericknmp@gmail.com>
2023-03-24 16:12:39 -0300

    test(typography): complement typography tests

--- src/components/__tests__/typography.test.tsx
+++ src/components/__tests__/typography.test.tsx
@@ -4,26 +4,96 @@ import { renderWithProviders } from '../../helpers/testing';
 import { Heading } from '../typography/heading';
 import { Text } from '../typography/text';
 
-const testID = 'heading-test';
+const testID = 'typography-test';
 
-describe('Typography > Heading', () => {
-	it('should render correctly', function () {
-		const { getByTestId } = renderWithProviders(
-			<Heading testID={testID}>Test</Heading>
-		);
+describe('Typography', () => {
+	describe('Heading', () => {
+		it('should render correctly', function () {
+			const { getByTestId } = renderWithProviders(
+				<Heading testID={testID}>Test</Heading>
+			);
 
-		expect(getByTestId(testID)).toBeTruthy();
-		expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+		});
+
+		it('should render correctly with color', function () {
+			const { getByTestId } = renderWithProviders(
+				<Heading testID={testID}>Test</Heading>
+			);
+
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toHaveStyle({ color: '#000000' });
+		});
+
+		it('should render custom correctly with color', function () {
+			const { getByTestId } = renderWithProviders(
+				<Heading testID={testID} color="brand.pure">
+					Test
+				</Heading>
+			);
+
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toHaveStyle({ color: '#6732D1' });
+		});
+
+		it('should render correctly with inverted', function () {
+			const { getByTestId } = renderWithProviders(
+				<Heading testID={testID} inverted>
+					Test
+				</Heading>
+			);
+
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toHaveStyle({ color: '#FFFFFF' });
+		});
 	});
-});
 
-describe('Typography > Text', () => {
-	it('should render correctly', function () {
-		const { getByTestId } = renderWithProviders(
-			<Text testID={testID}>Test</Text>
-		);
+	describe('Text', () => {
+		it('should render correctly', function () {
+			const { getByTestId } = renderWithProviders(
+				<Text testID={testID}>Test</Text>
+			);
 
-		expect(getByTestId(testID)).toBeTruthy();
-		expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+		});
+
+		it('should render correctly with color', function () {
+			const { getByTestId } = renderWithProviders(
+				<Text testID={testID}>Test</Text>
+			);
+
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toHaveStyle({ color: '#000000' });
+		});
+
+		it('should render custom correctly with color', function () {
+			const { getByTestId } = renderWithProviders(
+				<Text testID={testID} color="brand.pure">
+					Test
+				</Text>
+			);
+
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toHaveStyle({ color: '#6732D1' });
+		});
+
+		it('should render correctly with inverted', function () {
+			const { getByTestId } = renderWithProviders(
+				<Text testID={testID} inverted>
+					Test
+				</Text>
+			);
+
+			expect(getByTestId(testID)).toBeTruthy();
+			expect(getByTestId(testID)).toHaveTextContent('Test');
+			expect(getByTestId(testID)).toHaveStyle({ color: '#FFFFFF' });
+		});
 	});
 });
